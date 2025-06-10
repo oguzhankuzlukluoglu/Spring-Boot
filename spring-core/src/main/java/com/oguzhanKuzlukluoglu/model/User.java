d6532a5987b249dd13c8ed14249f730ce88d9d90
oguzhankuzlukluoglu <oguzhankuzlukluoglu@gmail.com>
2025-06-10 23:43:50 +0300

    feat: Initialize Spring Boot project with basic structure and dependencies
    - Added Maven Wrapper scripts for easier project setup.
    - Created pom.xml with Spring Boot dependencies and configuration.
    - Implemented main application class (SpringCoreApplication) for bootstrapping.
    - Added application.properties for application name configuration.
    - Created test class (SpringCoreApplicationTests) for context loading verification.
    - Included additional TestSinif class for demonstration purposes.
    - Configured .gitignore and .gitattributes for proper version control.

--- spring-core/src/main/java/com/oguzhanKuzlukluoglu/model/User.java
+++ spring-core/src/main/java/com/oguzhanKuzlukluoglu/model/User.java
@@ -1,4 +1,14 @@
 package com.oguzhanKuzlukluoglu.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private String firstname;
 }
---
+++ spring-core/src/test/java/com/oguzhanKuzlukluoglu/spring_core/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhanKuzlukluoglu.spring_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
